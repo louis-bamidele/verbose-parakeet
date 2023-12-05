e59d40eb67046817de85ffbcf98c5d3367bf8901
louis <louisbamidele@icloud.com>
2023-12-05 09:12:11 +0100

    loading re-edit

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,12 +3,17 @@ import React from "react";
 function loading() {
   return (
     <div className='absolute top-[0] left-[0] z-index-[99] w-[100%] h-[100%] grid place-items-center'>
-      <div className='spinner'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className='flex flex-col gap-6 justify-center items-center'>
+        <div className='spinner flex gap-3'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className='text '>
+          <h4>This weather app need access to your location.</h4>
+        </div>
       </div>
     </div>
   );
